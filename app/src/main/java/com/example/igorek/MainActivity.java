627f27d25853527fbe049f003cda10b746299158
IgorSukhovMaznin <igor.sukhovmaznin@gmail.com>
2022-12-27 18:59:09 +0300

    Наследование классов

--- app/src/main/java/com/example/igorek/MainActivity.java
+++ app/src/main/java/com/example/igorek/MainActivity.java
@@ -6,10 +6,25 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Cat myCat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        myCat = new Cat(3, "Barsik", "Ball", "red");
+        myCat.talk();
+
+        Cat murzik = new Cat(4, "Murzik", "Merzella", "yellow");
+        murzik.talk();
+
+        Cat glasha = new Cat();
+        glasha.age = 1;
+        glasha.name = "Glasha";
+        glasha.breed = "Golosha";
+        glasha.color = "black";
+        glasha.talk();
     }
 }
---
+++ app/src/main/java/com/example/igorek/Puma.java
@@ -0,0 +1,4 @@
+package com.example.igorek;
+
+public class Puma extends Cat {
+}
