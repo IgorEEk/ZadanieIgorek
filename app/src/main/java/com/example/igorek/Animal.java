ede3341686d01e5b928696c64453b097ca1042ff
IgorSukhovMaznin <igor.sukhovmaznin@gmail.com>
2022-12-27 19:29:38 +0300

    Абстрактные классы

--- app/src/main/java/com/example/igorek/Animal.java
+++ app/src/main/java/com/example/igorek/Animal.java
@@ -3,10 +3,12 @@ package com.example.igorek;
 
 import android.util.Log;
 
-public class Animal {
+public abstract class Animal {
     int numberOfLegs;
     boolean isAlive;
 
+
+    abstract public void draw();
 
     public String breathe() {
         return "I can breathe";
